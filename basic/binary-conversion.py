2fa0a676b28913053cfd7d0772298dedd3771a89
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-10-26 10:40:10 +0545

    Create binary-conversion.py

---
+++ basic/binary-conversion.py
@@ -0,0 +1,7 @@
+def to_binary(n):
+    return bin(n)[2:]
+
+# Example usage
+print(to_binary(5))    # Output: '101'
+print(to_binary(10))   # Output: '1010'
+print(to_binary(255))  # Output: '11111111'
